b7d4f187ebb90ac15dc5c3065dbc1209e6a6c9e3
marujoo <bernardo.escada@gmail.com>
2024-11-07 09:48:29 +0000

    eventos

--- lab7/main.js
+++ lab7/main.js
@@ -1,12 +1,14 @@
-const corTexto = document.getElementById('corTexto');
-const btnRed = document.getElementById('btnRed');
-const btnGreen = document.getElementById('btnGreen');
-const btnBlue = document.getElementById('btnBlue');
-const btnSubmit = document.getElementById('btnSubmit');
-const corInput = document.getElementById('corInput');
-const btnContador = document.getElementById('btnContador');
-const contador = document.getElementById('contador');
-const hoverText = document.getElementById('hoverText');
+const corTexto = document.querySelector('#corTexto');
+const btnRed = document.querySelector('#btnRed');
+const btnGreen = document.querySelector('#btnGreen');
+const btnBlue = document.querySelector('#btnBlue');
+const corInput = document.querySelector('#corInput');
+const btnContador = document.querySelector('#btnContador');
+const contador = document.querySelector('#contador');
+const hoverText = document.querySelector('#hoverText');
+const background = document.querySelector('#background');
+let random = 0;
+let count = 0;
 
 function mudarCor(color) {
     corTexto.style.color = color;
@@ -17,13 +19,11 @@ btnGreen.addEventListener('click', () => mudarCor('green'));
 btnBlue.addEventListener('click', () => mudarCor('blue'));
 
 function mudarCorDeFundo() {
-    const color = corInput.value; 
+    const color = document.querySelector('#corInput').value; 
     document.body.style.backgroundColor = color; 
 }
 
-btnSubmit.addEventListener('click', mudarCorDeFundo);
 
-let count = 0;
 btnContador.addEventListener('click', () => {
     count += 1;
     contador.textContent = count;
@@ -36,6 +36,17 @@ function textoHover() {
 function restaurarTexto() {
     hoverText.textContent = 'Passa por aqui';
 }
+
+function muda_aleatorio(){
+    random++;
+    if(random % 4 == 0) background.style.backgroundColor = 'grey'
+    if(random % 4 == 1) background.style.backgroundColor = 'lightblue'
+    if(random % 4 == 2) background.style.backgroundColor = 'salmon'
+    if(random % 4 == 3) background.style.backgroundColor = 'khaki'
+
+}
+
+background.addEventListener('keyup', muda_aleatorio);
 
 hoverText.addEventListener('mouseover', textoHover);
 hoverText.addEventListener('mouseout', restaurarTexto);
